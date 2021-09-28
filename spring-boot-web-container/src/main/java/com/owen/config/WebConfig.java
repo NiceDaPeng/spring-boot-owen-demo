73d5670fe7b4d43b25c83a7e3a4425b9615a7b33
pg-dpw <1632752770@qq.com>
2021-09-28 16:15:41 +0800

    spring-boot-owen-mybatisplus

---
+++ spring-boot-web-container/src/main/java/com/owen/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.owen.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> customizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.setPort(8899);
+            }
+        };
+    }
+}
