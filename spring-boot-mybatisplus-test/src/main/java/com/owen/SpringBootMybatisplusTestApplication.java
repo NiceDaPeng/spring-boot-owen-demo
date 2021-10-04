8e29a71f56924193d5dc01869e9ea713763b977e
pg-dpw <1632752770@qq.com>
2021-10-04 09:11:49 +0800

    mybatisplus test

---
+++ spring-boot-mybatisplus-test/src/main/java/com/owen/SpringBootMybatisplusTestApplication.java
@@ -0,0 +1,13 @@
+package com.owen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisplusTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisplusTestApplication.class, args);
+    }
+
+}
